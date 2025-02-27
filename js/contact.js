fd0a6928418324d46f71817beb4d702f1c3a3235
jasmin <yase0236@stud.kea.dk>
2025-02-27 08:26:48 +0100

    changes

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,3 @@
-// let myName = 'KEA';
-// const myOtherName = 'CPHBusiness';
-
 document.querySelector('#frmContact').addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -8,27 +5,24 @@ document.querySelector('#frmContact').addEventListener('submit', (e) => {
     const email = e.target.txtEmail.value;
     const comments = e.target.txtComments.value;
 
-    const submittedInfo = `Thank you for your message. We will get back to you shortly.
-    
-    Registered information:
-    - Name: ${name}
-    - Email: ${email}
-    - Comments: ${comments}
-    `;
-        
-    alert(submittedInfo);
+    const modal = document.querySelector("#contact_message");
+    modal.querySelector("#name").innerText = name;
+    modal.querySelector("#email").innerText = email;
+    modal.querySelector("#comments").innerText = comments;
 
-    window.location.replace('index.html');
+    modal.showModal();
+    modal.focus();
+
+    modal.querySelector(".close").addEventListener("click", function () {
+    this.parentElement.parentElement.close();
+    });
+
+    modal.addEventListener("close", () => {
+    window.location.replace("index.html");
+    });
+
+  // Faster and less cumbersome, but sometimes you don't have the element you want to operate with
+  // modal.querySelector('.close').addEventListener('click', () => {
+  //     modal.close();
+  // });
 });
-
-// function showText(text) {
-//     let intro = 'Your text is: ';
-
-//     let moreText = '';
-//     for (let index = 0; index < 10; index++) {
-//         moreText += index;
-//     }
-//     intro += text + moreText;
-//     alert(intro);
-// }
-// showText('Hello');
